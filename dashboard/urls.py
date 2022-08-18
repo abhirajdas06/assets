2e69c072b54c707f484414d2937b55841b9823d2
abhirajdas06 <abhirajdas57@gmail.com>
2022-08-18 18:50:12 +0530

    urls update

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from dashboard import views
 
 urlpatterns = [
     path('',views.index,name="Home"),
+    path('profile/', views.profile, name="Profile")
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
 
 # Create your views here.
 def index(request):
@@ -7,3 +9,9 @@ def index(request):
     #     print ('hello')
     # return HttpResponse("Hello Home")
     return render(request, "home/index.html")
+
+def profile(request):
+    # if request.method == 'PSOT':
+    #     print ('hello')
+    # return HttpResponse("Hello Home")
+    return render(request, "home/profile.html")
